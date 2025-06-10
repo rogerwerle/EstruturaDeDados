171e7ee8597921b0748a6994d0bdde918b7e5e68
rogerwerle <mwerleroger@gmail.com>
2025-06-10 15:15:04 -0300

    Organizando a classe Bubblesort

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -1,10 +1,9 @@
 public class Bubblesort {
     public static void main(String[] args) {
-        int num;
         int[] vetor = {5, 9, 3, 1, 6, 25, 8};
 
         System.out.print("Vetor Desordenado: ");
-        imprimirVetor(vetor);
+        Utils.imprimirVetor(vetor);
 
         /*
             Devido ao grande número de comparações e trocas que são realizadas, esse
@@ -12,6 +11,14 @@ public class Bubblesort {
             em vetores grandes.
         */
 
+        bubble(vetor);
+
+        System.out.print("Vetor ordenado com o método Bubblesort: ");
+        Utils.imprimirVetor(vetor);
+    }
+
+    public static void bubble(int[] vetor) {
+        int num;
         for (int i = 0; i < vetor.length; i++) {
             for (int j = i + 1; j < vetor.length; j++) {
                 if (vetor[i] > vetor[j]) {
@@ -21,14 +28,5 @@ public class Bubblesort {
                 }
             }
         }
-        System.out.print("Vetor ordenado com o método Bubblesort: ");
-        imprimirVetor(vetor);
-    }
-
-    public static void imprimirVetor(int[] vetor) {
-        for (int i : vetor) {
-            System.out.print(i+ " ");
-        }
-        System.out.println();
     }
 }
