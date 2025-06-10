c9ef15c97e654c82b56dca4d6ccfb7d08bbbd985
rogerwerle <mwerleroger@gmail.com>
2025-06-10 15:14:03 -0300

    criacao da classe utils para mostrar os vetores

---
+++ src/Utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+
+    public static void imprimirVetor(int[] vetor) {
+        for (int i : vetor) {
+            System.out.print(i+ " ");
+        }
+        System.out.println();
+    }
+}
